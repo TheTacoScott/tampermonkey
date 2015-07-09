f21a4effeca26a799a28a182d5dc8026dfb124cd
Scott Powers <scott.powers@untsystem.edu>
2015-07-09 10:55:55 -0500

    tweaking the player to launch vlc

--- linux-media-helper.sh
+++ linux-media-helper.sh
@@ -7,11 +7,11 @@ do
   tester=`echo $clip | grep -Eui "^(SELE|PLAY|MOVE|MOVV|RENA|DELE):" | wc -l`
   if [ $tester -gt 0 ]
   then
-    action=`echo "$clip" | cut -d" " -f1 | cut -d":" -f1`
+    action=`echo "$clip" | cut -d" " -f1 | cut -d":" -f1 | tr -d '\n' | tr -d '\r'`
     justclip=`echo "$clip" | cut -d" " -f2 | cut -d"|" -f1`
     uncoded=`echo "$justclip" | python -c 'import sys, urllib as ul; print ul.unquote_plus(sys.stdin.read())'`
     cleanedup=`echo "$uncoded" | sed "s/z://g" | tr '\\' '/' 2>/dev/null`
-    final=`echo "$prefix$cleanedup" | sed "s/\/\//\//g"`
+    final=`echo "$prefix$cleanedup" | sed "s/\/\//\//g" | tr -d "\n" | tr -d "\r"`
     if [ "$action" == "SELE" ]
     then
       echo "$action -- $final"
@@ -19,10 +19,12 @@ do
     elif [ "$action" == "PLAY" ]
     then
       echo "$action -- $final"
+      vlc "$final"
+      echo "VLC EXIT" 
     else
       echo "$action -- $selection"
     fi
-    xclip -selection clipboard -i /dev/null
+    dump=`xclip -selection clipboard -i /dev/null`
   fi
   sleep 0.05
 done
